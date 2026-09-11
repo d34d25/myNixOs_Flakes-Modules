8a35fbd9ff7453bbccb6e2142027f5f88fc5b308
d34d25 <pvtS3ns3i@proton.me>
2026-09-11 16:13:17 -0300

    removed comments from configuration.nix

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, pkgs, ... }:
 
 {
@@ -51,11 +47,6 @@
   };
 
   nixpkgs.config.allowUnfree = true;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.

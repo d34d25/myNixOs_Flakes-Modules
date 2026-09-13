a95793f44be317f0745173cc628e5f29e2d468e1
d34d25 <pvtS3ns3i@proton.me>
2026-09-13 18:42:42 -0300

    simplified nix configuration

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,70 +1,93 @@
-{ config, pkgs, ... }:
+{ config, pkgs, stable, unstable, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-    ];
+  	imports = [./hardware-configuration.nix];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  	# Use the systemd-boot EFI boot loader.
+  	boot.loader.systemd-boot.enable = true;
+  	boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "dision";
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  	#networking
+  	networking.hostName = "dision";
+ 
+  	networking.networkmanager.enable = true;
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+	networking.firewall.enable = true;
 
-  time.timeZone = "America/Argentina/Buenos_Aires";
+  	#time zone / locale
 
-  i18n.defaultLocale = "en_US.UTF-8";
+  	time.timeZone = "America/Argentina/Buenos_Aires";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
+  	i18n.defaultLocale = "en_US.UTF-8";
 
-  services.xserver.xkb = {
-    layout = "latam";
-    variant = "";
-  };
+  	i18n.extraLocaleSettings = {
 
-  console.keyMap = "la-latin1";
+    		LC_ADDRESS = "en_US.UTF-8";
+    		LC_IDENTIFICATION = "en_US.UTF-8";
+    		LC_MEASUREMENT = "en_US.UTF-8";
+    		LC_MONETARY = "en_US.UTF-8";
+    		LC_NAME = "en_US.UTF-8";
+    		LC_NUMERIC = "en_US.UTF-8";
+    		LC_PAPER = "en_US.UTF-8";
+    		LC_TELEPHONE = "en_US.UTF-8";
+    		LC_TIME = "en_US.UTF-8";
 
-  users.users."dision" = {
-    isNormalUser = true;
-    description = "dision";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
-  };
+  	};
 
-  nixpkgs.config.allowUnfree = true;
+  	services.xserver.xkb = {
+    		layout = "latam";
+    		variant = "";
+  	};
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "26.05"; # Did you read the comment?
+  	console.keyMap = "la-latin1";
 
+  	#users
+
+  	users.users."dision" = {
+    		isNormalUser = true;
+    		description = "dision";
+    		extraGroups = [ "networkmanager" "wheel" ];
+    		packages = with pkgs; [];
+  	};
+
+  	nixpkgs.config.allowUnfree = true;
+
+  	system.stateVersion = "26.05"; #<--- do not touch
+
+	nix.settings.experimental-features = ["nix-command" "flakes"];
+
+	#sound
+
+	security.rtkit.enable = true;
+
+	services.pipewire = {
+
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+
+	};
+
+
+  	#programs
+
+	programs.vscode = {
+
+		enable = true;
+		package = stable.vscode;
+		extensions = [stable.vscode-extensions.ms-vscode.cpptools];
+
+	};	
+	
+	environment.systemPackages = [
+	
+		stable.git
+		stable.htop
+		stable.fastfetch
+		stable.aseprite
+		stable.blender
+		stable.librewolf	
+
+	];  
 }
--- system/flake.nix
+++ system/flake.nix
@@ -1,55 +1,53 @@
 {
 	description = "flake for the system";
-	
+
 	inputs = {
+		nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-26.05";
+
+		nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+	};
 	
-			nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-26.05";
-
-    		nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
- 	 };
-
-  	outputs = {self, nixpkgs-stable, nixpkgs-unstable, ...} @inputs:
-  	let
+	outputs = {self, nixpkgs-stable, nixpkgs-unstable, ...} @inputs:
+	let
 		system = "x86_64-linux";
 
-		commonModules = [
-			./configuration.nix
-		];
+		stable = import nixpkgs-stable {inherit system; config.allowUnfree = true;};
 
-		stable = import nixpkgs-stable {
-
-			inherit system;
-
-			config.allowUnfree = true;
-
-		};
-
-		unstable = import nixpkgs-unstable {
-
-			inherit system;
-
-			config.allowUnfree = true;
-
-		};
-
-  	in
-  	{
+		unstable = import nixpkgs-unstable {inherit system; config.allowUnfree = true;};
 	
-		nixosConfigurations.desktop = nixpkgs-stable.lib.nixosSystem {
+	in
+	{
 
+		nixosConfigurations.desktop = nixpkgs-stable.lib.nixosSystem {
+	
 			specialArgs = {inherit inputs stable unstable;};
 
-			modules = commonModules ++ [
-				./myModules/sys-settings.nix
-				./myModules/programs.nix
-
+			modules = [
+				
+				({config, ...}:
 				{
-					mySystem.enableNvidia = true;
-					mySystem.chosenDesktop = "kde";
-				}
-			];
+					services.xserver.enable = true;
 
+					services.xserver.videoDrivers = ["nvidia"];
+
+					hardware.nvidia = {
+					
+						open = false;
+						package = config.boot.kernelPackages.nvidiaPackages.production;
+
+					};
+
+					services.xserver.displayManager.lightdm.enable = true;
+					services.xserver.desktopManager.xfce.enable = true;
+
+				})
+				
+				./configuration.nix
+			];
+		
 		};
 
-  	};
+	};
+	
+	
 }

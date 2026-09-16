94d00704f30277043de5bfe64ae6b5353df29650
d34d25 <pvtS3ns3i@proton.me>
2026-09-16 13:50:50 -0300

    improved indentation in raylib's flake

--- dev_environments/raylib/flake.nix
+++ dev_environments/raylib/flake.nix
@@ -1,51 +1,51 @@
 #from: https://github.com/raysan5/raylib/wiki/Working-on-GNU-Linux
 
 {
-  description = "Raylib development environment";
+	description = "Raylib development environment";
 
-  inputs = {
+  	inputs = {
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  };
+  	};
 
-  outputs = {self, nixpkgs, ...}:
-  let
+  	outputs = {self, nixpkgs, ...}:
+  	let
 
-    system = "x86_64-linux";
+    		system = "x86_64-linux";
 
-    pkgs = import nixpkgs {inherit system;};
+    		pkgs = import nixpkgs {inherit system;};
 
-  in
-  {
+  	in
+  	{
 
-    devShells."${system}".default = pkgs.mkShell {
+    		devShells."${system}".default = pkgs.mkShell {
 
-        packages = [
+        		packages = [
 
-          #my additions
-          pkgs.raylib
-          pkgs.gcc
-          pkgs.gdb
-          pkgs.python3
+          			#my additions
+          			pkgs.raylib
+          			pkgs.gcc
+          			pkgs.gdb
+          			pkgs.python3
+				#------------
 
-          pkgs.libGL
+          			pkgs.libGL
 
-          # X11 dependencies
-          pkgs.libx11
-          pkgs.libx11.dev
-          pkgs.libxcursor
-          pkgs.libxi
-          pkgs.libxinerama
-          pkgs.libxrandr
+          			# X11 dependencies
+          			pkgs.libx11
+          			pkgs.libx11.dev
+          			pkgs.libxcursor
+          			pkgs.libxi
+          			pkgs.libxinerama
+          			pkgs.libxrandr
 
-          pkgs.emscripten
+          			pkgs.emscripten
 
-        ];
+        		];
 
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.alsa-lib];
+        		LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.alsa-lib];
+		};
 
-      };
-
-  };
+  	};
 }

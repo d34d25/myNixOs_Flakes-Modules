88596b6508e6a0bf649e0f9c5e860fd56b040b0a
d34d25 <pvtS3ns3i@proton.me>
2026-09-16 14:12:39 -0300

    removed spaces from raylib's flake

--- dev_environments/raylib/flake.nix
+++ dev_environments/raylib/flake.nix
@@ -4,18 +4,14 @@
 	description = "Raylib development environment";
 
   	inputs = {
-
     		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
   	};
 
   	outputs = {self, nixpkgs, ...}:
   	let
-
     		system = "x86_64-linux";
 
     		pkgs = import nixpkgs {inherit system;};
-
   	in
   	{
 
